1713d2675ae0376fc4cfda3b5acc270d886a86df
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2023-02-17 01:06:40 +1100

    Added User and Thought model

---
+++ models/Thought.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema (
+    {
+
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+    },
+
+    username: {
+        type: String,
+        required: true,
+    },
+
+    reactions: [reactionSchema]
+    }
+);
+
+const Thought= model('Thought', thoughtSchema);
+
+module.exports= Thought;
